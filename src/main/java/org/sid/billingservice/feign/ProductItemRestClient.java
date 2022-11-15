42896105527e694428df9bc0ef84dae44ea81e5d
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-11-15 23:35:13 +0100

    Initial commit

---
+++ src/main/java/org/sid/billingservice/feign/ProductItemRestClient.java
@@ -0,0 +1,19 @@
+package org.sid.billingservice.feign;
+
+import org.sid.billingservice.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@FeignClient(name = "PRODUCT-SERVICE")         //Pour pouvoir communiquer avec les autres micro services
+public interface ProductItemRestClient {
+    @GetMapping(path = "/products")
+    PagedModel<Product> pageProducts(@RequestParam(value = "page", defaultValue = "0") int page, @RequestParam(value = "size" ,defaultValue = "7") int size);
+
+    @GetMapping(path = "/products/{id}")
+    Product getProductById(@PathVariable Long id);
+}
---
+++ src/main/java/org/sid/billingservice/repositories/BillRepository.java
@@ -0,0 +1,12 @@
+package org.sid.billingservice.repositories;
+
+import org.sid.billingservice.entities.Bill;
+import org.sid.billingservice.entities.ProductItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.Collection;
+
+@RepositoryRestResource
+public interface BillRepository extends JpaRepository<Bill,Long> {
+}
